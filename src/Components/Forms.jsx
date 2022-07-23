3e6bd5ef2eb49d08eeb0cbdc571c8cac39ab1e3f
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-24 01:09:33 +0530

    details

---
+++ src/Components/Forms.jsx
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
+
+export const Forms =() => {
+
+    const [age, setAge] = React.useState('');
+    const [open, setOpen] = React.useState(false);
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    
+
+    return (
+      
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+
+        <TextField id="outlined-search" label="Candidate Name" type="search" />
+
+        <TextField id="outlined-search" label="Candidate Email" type="search" />
+        
+        <TextField
+          id="outlined-number"
+          label="Candidate Number"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+
+        <TextField id="outlined-search" label="Source" type="search" />
+
+        <TextField id="outlined-search" label="Previous Organization" type="search" />
+
+        <TextField id="outlined-search" label="Referral Employee Email" type="search" />
+
+        <TextField id="outlined-search" label="Skill" type="search" />
+
+        <TextField id="outlined-search" label="Total Experience" type="search" />
+
+        <TextField id="outlined-search" label="NP" type="search" />
+
+        <TextField id="outlined-search" label="Status" type="search" />
+
+
+        <div>
+
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-controlled-open-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      </div>
+
+      <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-controlled-open-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      </div>
+        
+      </div>
+     
+    </Box>
+  );
+}
+
+
+
